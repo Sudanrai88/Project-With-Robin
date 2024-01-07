56dc4baafa1f6245ae6aa78b57fc4de8e00bdc66
Sudanrai88 <101595305+Sudanrai88@users.noreply.github.com>
2024-01-07 17:12:43 +0000

    Created new App for project

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/navbar.js'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Navbar />
+      <Navbar />
+    </div>
+
+
+  );
+}
+
+export default App;
---
+++ my-app/src/Components/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function navbar(color) {
+  return (
+    <div>
+      <p>
+        HELLO
+      </p>
+    </div>
+  )
+}
+
+export default navbar
