8e8020f4fb2e97a4a4d7d60dee403373228652c2
robinrai1349 <robinrai1349@hotmail.com>
2024-01-07 18:17:39 +0000

    Componen

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/navbar.js'
+import Hero from './Components/Hero.js';
 
 function App() {
 
   return (
     <div className="App">
       <Navbar />
-      <Navbar />
+      <Hero />
     </div>
 
 
